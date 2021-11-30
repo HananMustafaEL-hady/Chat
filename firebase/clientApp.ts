d131e7e403a66fd7fc0862a85894101495a7b573
HananMustafaEL-hady <hananmustafa.hassan@gmail.com>
2021-11-30 21:01:05 +0200

    add delete function

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -4,13 +4,13 @@ import "firebase/firestore";
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+      apiKey: "AIzaSyAqMrt89i57gSGiGqtLryaXG1czcty2ERA",
+      authDomain: "chatapp-bebb5.firebaseapp.com",
+      projectId: "chatapp-bebb5",
+      storageBucket: "chatapp-bebb5.appspot.com",
+      messagingSenderId: "745924671349",
+      appId: "1:745924671349:web:f037e7fbe9d6a646e3853c",
+      measurementId: "G-CYSPXVBHBG",
     });
   } else {
     firebase.app();
